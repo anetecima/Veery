e0cec506a92c7f2fc74de3bf79806d71fa62f951
anetecima <a.cima@dynatech.lv>
2023-06-03 16:44:44 +0300

    init commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { MainPage } from './pages/main/mainPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/pages/main/mainPage.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled'
+const LogoStyle = styled.div`
+  width: 50%;
+  aspect-ratio: 1;
+  background: url('src/assets/veery.jpg') no-repeat center;
+  background-size: cover;
+`
+const PageStyle = styled.div`
+  width: 100%;
+  min-width: 100vw;
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const MainPage = () => (
+  <>
+    <PageStyle>
+      <LogoStyle />
+
+      <div>VEERY</div>
+    </PageStyle>
+  </>
+)
