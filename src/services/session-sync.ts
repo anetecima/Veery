e80a3cb2674a0a46ead2682493a3dd1d95a30346
anetecima <a.cima@dynatech.lv>
2023-06-05 02:01:36 +0300

    unused

--- src/services/session-sync.ts
+++ src/services/session-sync.ts
@@ -1,36 +1,36 @@
-import { Subject } from "rxjs";
+import { Subject } from 'rxjs'
 
 export class SessionSync {
   private readonly socket: WebSocket
 
-  midiData = new Subject<any>();
+  midiData = new Subject<any>()
 
   constructor(uri: string) {
     this.socket = new WebSocket(uri)
 
-    this.socket.addEventListener('open', (event) => {
-      console.log('WebSocket connection established');
-    });
+    this.socket.addEventListener('open', () => {
+      console.log('WebSocket connection established')
+    })
 
-    this.socket.onmessage =  (event) => {
+    this.socket.onmessage = (event) => {
       // Assuming the message is a Blob representing the MIDI file
       console.log('Received MIDI file:', event.data)
       const midiFileBlob = event.data
       // Generate a unique filename using UUID
 
-      console.log('midi file: ', midiFileBlob);
+      console.log('midi file: ', midiFileBlob)
 
       this.midiData.next(midiFileBlob)
-      
+
       // Handle the incoming message as needed
     }
 
-    this.socket.onclose = function (event) {
+    this.socket.onclose = function () {
       console.log('WebSocket connection closed.')
     }
 
-    this.socket.onerror = (e)=>{
-      console.log('error in ws connection: ', e);
+    this.socket.onerror = (e) => {
+      console.log('error in ws connection: ', e)
     }
   }
 
