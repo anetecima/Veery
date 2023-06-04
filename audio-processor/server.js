f4831e4e4ec59cc69eec53a4cc55df83747075a0
eyupdzhanY1 <eyupdzhanY1@verifone.com>
2023-06-04 19:43:34 +0300

    blobs

--- audio-processor/server.js
+++ audio-processor/server.js
@@ -1,85 +1,87 @@
-const WebSocket = require('ws');
-const fs = require('fs');
-const { spawn } = require('child_process');
-const { v4: uuidv4 } = require('uuid');
+const WebSocket = require('ws')
+const fs = require('fs')
+const { spawn } = require('child_process')
+const { v4: uuidv4 } = require('uuid')
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on('connection', (ws) => {
-  console.log('A client connected');
+  console.log('A client connected')
 
   ws.on('message', (message) => {
     // Assuming the message is an audio blob received from the client
-    const audioBlob = Buffer.from(message);
+    const audioBlob = Buffer.from(message)
 
     // Generate a unique filename using UUID
-    const uniqueFileName = `${uuidv4()}.wav`;
+    const uniqueFileName = `${uuidv4()}.oog`
 
     // Save the audio blob to a WAV file with the unique filename
-    const wavFilePath = uniqueFileName;
-    fs.writeFileSync(wavFilePath, audioBlob);
+    const oogFilePath = uniqueFileName
+    fs.writeFileSync(oogFilePath, audioBlob)
 
     // Convert the WAV file to MP3 using ffmpeg
-    const mp3FilePath = `${uniqueFileName}.mp3`;
+    const mp3FilePath = `${uniqueFileName}.mp3`
     const ffmpegProcess = spawn('ffmpeg', [
       '-i',
-      wavFilePath,
+      oogFilePath,
       '-codec:a',
       'libmp3lame',
       '-qscale:a',
       '2',
       mp3FilePath,
-    ]);
+    ])
 
     ffmpegProcess.on('close', (code) => {
       if (code === 0) {
-        console.log('Audio conversion completed successfully.');
+        console.log('Audio conversion completed successfully.')
 
         // Read the converted MP3 file
-        const mp3Data = fs.readFileSync(mp3FilePath);
+        const mp3Data = fs.readFileSync(mp3FilePath)
 
         // Establish a WebSocket connection to the other server
-        const otherServerWebSocket = new WebSocket('ws://127.0.0.1:8775');
+        const otherServerWebSocket = new WebSocket('ws://127.0.0.1:8775')
 
         otherServerWebSocket.on('open', () => {
           // Send the MP3 data to the other server
-          otherServerWebSocket.send(mp3Data);
+          otherServerWebSocket.send(mp3Data)
 
-          console.log('MP3 data sent to the other server.');
+          console.log('MP3 data sent to the other server.')
 
           // Close the WebSocket connection
-          otherServerWebSocket.close();
+          otherServerWebSocket.close()
 
           // Remove temporary files
-          fs.unlinkSync(wavFilePath);
-          fs.unlinkSync(mp3FilePath);
-        });
+          fs.unlinkSync(oogFilePath)
+          fs.unlinkSync(mp3FilePath)
+        })
 
-        otherServerWebSocket.on('message', (data)=>{
-            console.log('message from server: ', data);
+        otherServerWebSocket.on('message', (data) => {
+          console.log('message from server: ', data)
         })
 
         otherServerWebSocket.on('error', (error) => {
-          console.error('Error occurred while connecting to the other server:', error);
-          
+          console.error(
+            'Error occurred while connecting to the other server:',
+            error
+          )
 
-        //   // Remove temporary files
-        //   fs.unlinkSync(wavFilePath);
-        //   fs.unlinkSync(mp3FilePath);
-        });
+          //   // Remove temporary files
+          //   fs.unlinkSync(oogFilePath);
+          //   fs.unlinkSync(mp3FilePath);
+        })
       } else {
-        console.error('Audio conversion failed with code:', code);
+        console.error('Audio conversion failed with code:', code)
 
         // // Remove temporary files
-        // fs.unlinkSync(wavFilePath);
+        // fs.unlinkSync(oogFilePath);
         // fs.unlinkSync(mp3FilePath);
       }
-    });
-  });
+    })
+  })
 
   ws.on('close', () => {
-    console.log('A client disconnected');
-  });
-});
+    console.log('A client disconnected')
+  })
+})
 
 console.log('server running on 8080')
--- src/services/session-sync.ts
+++ src/services/session-sync.ts
@@ -1,47 +1,41 @@
-import { v4 as uuidv4 } from 'uuid';
-import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid'
+import fs from 'fs'
 
+export class SessionSync {
+  private readonly socket: WebSocket
 
-export class SessionSync{
+  constructor(uri: string) {
+    this.socket = new WebSocket(uri)
 
-    private readonly socket:WebSocket; 
-
-    constructor(uri: string){
-        this.socket = new WebSocket(uri);
-
-         // Event handler for successful connection
-         this.socket.onopen = function(event) {
-            console.log('WebSocket connection established.', event);
-        };
-
-        this.socket.onmessage = function(event) {
-
-             // Assuming the message is a Blob representing the MIDI file
-             const midiFileBlob = event.data;
-             // Generate a unique filename using UUID
-             const uniqueFileName = `${uuidv4()}.mid`;
-
-             // Save the MIDI file with the unique filename
-             fs.writeFile(uniqueFileName, midiFileBlob, (error) => {
-                 if (error) {
-                     console.error('Error saving MIDI file:', error);
-                    } else {
-                    console.log('MIDI file saved successfully:', uniqueFileName);
-                    }
-                });                    
-            // Handle the incoming message as needed
-        };
-
-        this.socket.onclose = function(event) {
-            console.log('WebSocket connection closed.');
-        };
+    // Event handler for successful connection
+    this.socket.onopen = function (event) {
+      console.log('WebSocket connection established.', event)
     }
 
-    
+    // this.socket.onmessage = function(event) {
 
-    sendAudioData(data: Blob): void{
-        this.socket.send(data);
+    //      // Assuming the message is a Blob representing the MIDI file
+    //      const midiFileBlob = event.data;
+    //      // Generate a unique filename using UUID
+    //      const uniqueFileName = `${uuidv4()}.mid`;
+
+    //      // Save the MIDI file with the unique filename
+    //      fs.writeFile(uniqueFileName, midiFileBlob, (error) => {
+    //          if (error) {
+    //              console.error('Error saving MIDI file:', error);
+    //             } else {
+    //             console.log('MIDI file saved successfully:', uniqueFileName);
+    //             }
+    //         });
+    //     // Handle the incoming message as needed
+    // };
+
+    this.socket.onclose = function (event) {
+      console.log('WebSocket connection closed.')
     }
+  }
 
-
+  sendAudioData(data: Blob): void {
+    this.socket.send(data)
+  }
 }
